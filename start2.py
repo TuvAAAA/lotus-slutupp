b9caf526ab9af75f326cdd7296211edd10466a37
TuvAAAA <tuva.tenghall@elev.ga.ntig.se>
2025-12-16 13:28:27 +0100

    ....delete saken dunkar inte, jag tänker ditcha den iden och ksks göra lite mer på den

--- start2.py
+++ start2.py
@@ -12,18 +12,20 @@ def slump(event=None):
     value = random.randint(1,6)
     mapping = {
         1: "hoppa",
-        2: "japp",
-        3: "tihi",
-        4: "jahopp",
-        5: "raaaa",
-        6: "lalala",
+        2: "snurra 3 gånger",
+        3: "krama någon",
+        4: "rita ett porträtt av din vän",
+        5: "ta ett varv runt skolan",
+        6: "...67",
     }
     text = mapping.get(value, str(value))
     
-    output.config(state="normal")
+    output.configure(state= tk.NORMAL)
     output.insert(tk.END, text + "\n")
     output.see(tk.END)
-    output.config(state="disabled")
+    output.configure(state="disabled")
+    output.delete('1.0', tk.END)
+
 
 output = tk.Text(main,height=10, width=32, state="disabled")
 output.grid()
@@ -31,4 +33,5 @@ output.grid()
 add_button = tk.Button(main,text="random", command=slump)
 add_button.grid()
 
+clicked = output.bind("<Button-1>", slump)
 main.mainloop()
